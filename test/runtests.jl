b961168b59b3ddfd4333745d7d309a5437b8dda6
Mahdi Zakizadeh <mahdizakizadeh.me@gmail.com>
2019-03-27 17:06:46 +0430

    add tests for fixed size strings

--- test/runtests.jl
+++ test/runtests.jl
@@ -35,90 +35,98 @@ k, v = Foo(rand(Int64), rand(Int64)), rand(Int64)
 put!(s6, k, v)
 @test get(s6, k) == v
 
+# test using FixedSizeStrings in tree
+# first you should get and use FixedSizeStrings.jl package
+# using FixedSizeStrings
+# s7 = Store{Int64, FixedSizeString{96}}()
+# k, v = rand(Int64), randstring(96)
+# put!(s7, k, v)
+# @test get(s7, k) == v
+
 # Test some behaviors of tree
 # transfrerring data to next level
-s7 = Store{Int64, Int64}(80, 160, 2)
+s8 = Store{Int64, Int64}(80, 160, 2)
 for i in 1:5
-    put!(s7, i, i)
+    put!(s8, i, i)
 end
-@test length(s7.buffer.entries) == 0
-@test length(s7.levels[1]) == 5
+@test length(s8.buffer.entries) == 0
+@test length(s8.levels[1]) == 5
 for i in 6:10
-    put!(s7, i , i)
+    put!(s8, i , i)
 end
-@test length(s7.buffer.entries) == 0
-@test length(s7.levels[1]) == 10
+@test length(s8.buffer.entries) == 0
+@test length(s8.levels[1]) == 10
 for i in 11:20
-    put!(s7, i, i)
+    put!(s8, i, i)
 end
-@test length(s7.buffer.entries) == 0
-@test length(s7.levels[1]) == 10
-@test length(s7.levels[2]) == 10
+@test length(s8.buffer.entries) == 0
+@test length(s8.levels[1]) == 10
+@test length(s8.levels[2]) == 10
 for i in 21:35
-    put!(s7, i, i)
+    put!(s8, i, i)
 end
-@test length(s7.buffer.entries) == 0
-@test length(s7.levels[1]) == 5
-@test length(s7.levels[2]) == 10
-@test length(s7.levels[3]) == 20
+@test length(s8.buffer.entries) == 0
+@test length(s8.levels[1]) == 5
+@test length(s8.levels[2]) == 10
+@test length(s8.levels[3]) == 20
 
 # partitioning levels
-s8 = Store{Int8, Int8}(20, 40, 2, 20)
+s9 = Store{Int8, Int8}(20, 40, 2, 20)
 for i in 1:20
-    put!(s8, i, i)
+    put!(s9, i, i)
 end
-@test length(s8.levels[1].tables) == 2
+@test length(s9.levels[1].tables) == 2
 for i in 21:30
-    put!(s8, i, i)
+    put!(s9, i, i)
 end
 # test if tables can compact correcly in next level
-@test length(s8.levels[1].tables) == 1
-@test length(s8.levels[2].tables) == 2
+@test length(s9.levels[1].tables) == 1
+@test length(s9.levels[2].tables) == 2
 
 # Test methods
 # put (and update)
-s9 = Store{Int64, Int64}(32, 64, 2)
-put!(s9, 1, 1)
-put!(s9, 1, 2)
-# test entries replacement in buffer
-@test length(s9.buffer.entries) == 1
-@test get(s9, 1) == 2
-put!(s9, 2, 2)
-put!(s9, 1, 3)
-# test the hierarchy of get function
-@test get(s9, 1) == 3
-put!(s9, 2, 3)
-# test entries replacement in levels 
-@test s9.levels[1].size == 2 * 16
-
-# delete
 s10 = Store{Int64, Int64}(32, 64, 2)
 put!(s10, 1, 1)
-delete!(s10, 1)
-@test get(s10, 1) == nothing
-@test_throws AssertionError delete!(s10, 2)
-# test force remove in last level
+put!(s10, 1, 2)
+# test entries replacement in buffer
+@test length(s10.buffer.entries) == 1
+@test get(s10, 1) == 2
+put!(s10, 2, 2)
+put!(s10, 1, 3)
+# test the hierarchy of get function
+@test get(s10, 1) == 3
+put!(s10, 2, 3)
+# test entries replacement in levels 
+@test s10.levels[1].size == 2 * 16
+
+# delete
 s11 = Store{Int64, Int64}(32, 64, 2)
 put!(s11, 1, 1)
-put!(s11, 2, 2)
-put!(s11, 3, 3)
 delete!(s11, 1)
-@test s11.levels[1].size == 3 * 16
 @test get(s11, 1) == nothing
+@test_throws AssertionError delete!(s11, 2)
+# test force remove in last level
+s12 = Store{Int64, Int64}(32, 64, 2)
+put!(s12, 1, 1)
+put!(s12, 2, 2)
+put!(s12, 3, 3)
+delete!(s12, 1)
+@test s11.levels[1].size == 3 * 16
+@test get(s12, 1) == nothing
 
 # iteration
-s12 = Store{Int128, Int128}(400000, 1000000, 4, 100000)
+s13 = Store{Int128, Int128}(400000, 1000000, 4, 100000)
 ints = []
 for i in 1:100000
     x = rand(Int128)
     # x = i
     push!(ints, x)
-    put!(s12, x, i)
+    put!(s13, x, i)
 end
 sort!(ints)
-state = iter_init(s12)
+state = iter_init(s13)
 for i in 1:100000
-    done, p = iter_next(s12, state)
+    done, p = iter_next(s13, state)
     @test ints[i] == p[1]
     # @test i == 100000 && done
 end
