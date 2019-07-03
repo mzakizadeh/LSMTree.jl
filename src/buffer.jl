a624846ff733d1679f34a1281b45fde39fac6196
Mahdi Zakizadeh <mahdizakizadeh.me@gmail.com>
2019-07-03 11:07:42 +0430

    Change buffer ds to dict

--- src/buffer.jl
+++ src/buffer.jl
@@ -1,48 +1,36 @@
 mutable struct Buffer{K, V}
-    bf::Blob{BloomFilter}
     size::Integer
     max_size::Integer
-    entries::Vector{Entry{K, V}}
+    entries::Dict{K, Entry{K, V}}
     function Buffer{K, V}(max_size::Integer) where {K, V} 
-        bf = Blobs.malloc_and_init(BloomFilter, max_size, 0.001, 10)
-        new{K, V}(bf, 0, max_size, Vector{Entry{K, V}}())
+        new{K, V}(0, max_size, Dict{K, Entry{K, V}}())
     end
 end
 
 isfull(b::Buffer) = b.size >= b.max_size
 
 function Base.empty!(b::Buffer{K, V}) where {K, V}
-    b.entries = Vector{Entry{K, V}}()
+    empty!(b.entries)
     b.size = 0
 end
 
 function Base.put!(b::Buffer{K, V}, key, val, deleted=false) where {K, V}
-    # if in(key, b.bf[])
-    #     sort!(b.entries)
-    #     i = bsearch(b.entries, 1, length(b.entries), key)
-    #     i > 0 && deleteat!(b.entries, i)
-    # end
-    e = Entry{K, V}(key, val, deleted)
-    b.size += 1
-    # add!(b.bf[], key)
-    push!(b.entries, e)
+    b.size += haskey(b.entries, key) ? 0 : 1
+    b.entries[key] = Entry(key, val, deleted)
 end
 
 function Base.get(b::Buffer{K, V}, key) where {K, V}
-    !in(b.bf[], key) && nothing
-    sort!(b.entries)
-    i = bsearch(b.entries, 1, length(b.entries), key)
-    i > 0 ? (isdeleted(b.entries[i]) ? nothing : b.entries[i].val) : nothing
+    return b.entries[key]
 end
 
 function to_blob(b::Buffer{K, V}) where {K, V} 
-    sort!(b.entries)
-    size = length(b.entries)
-    bv = Blobs.malloc_and_init(BlobVector{Entry{K, V}}, size)
-    for i in 1:size
-        bv[i].key[] = b.entries[i].key
-        bv[i].val[] = b.entries[i].val
-        bv[i].deleted[] = b.entries[i].deleted
+    c = collect(b.entries)
+    sort!(c)
+    bv = Blobs.malloc_and_init(BlobVector{Entry{K, V}}, b.size)
+    for i in 1:b.size
+        bv[i].key[] = c[i][1]
+        bv[i].val[] = c[i][2].val
+        bv[i].deleted[] = c[i][2].deleted
     end
     return bv
 end
